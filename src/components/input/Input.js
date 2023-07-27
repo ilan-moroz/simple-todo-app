d0ac170ef1c6ff9290862c434f226549d4e02109
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-07-27 20:32:46 +0300

    on submit

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -2,15 +2,16 @@ import "./input.css";
 
 export const Input = props => {
   return (
-    <div class="form__group">
+    <div className="form__group">
       <input
         type={props.type}
         className="form__input"
         placeholder={props.placeholder}
         id={props.id}
+        onChange={props.onChange}
         required
       />
-      <label htmlFor={props.id} class="form__label">
+      <label htmlFor={props.id} className="form__label">
         {props.placeholder}
       </label>
     </div>
--- src/components/userForm/UserForm.js
+++ src/components/userForm/UserForm.js
@@ -1,18 +1,53 @@
 import { Input } from "../../components/input/Input";
 import { Button } from "../button/Button";
 import "./userForm.css";
+import React from "react";
+
+const initialState = {
+  task: "",
+  date: "",
+  time: "",
+};
 
 export const UserForm = () => {
+  const [userInputs, setUserInputs] = React.useState(initialState);
+
+  const handleChange = (key, value) => {
+    setUserInputs(prevInputs => ({
+      ...prevInputs,
+      [key]: value,
+    }));
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log(userInputs);
+  };
+
   return (
     <div className="form-box">
-      <form action="#" className="form">
+      <form onSubmit={onSubmit} className="form">
         <Input
           type="text"
           placeholder="Enter task and choose finish date/time: "
-          id="name"
+          id="task"
+          value={userInputs.task}
+          onChange={e => handleChange("task", e.target.value)}
         />
-        <Input type="date" placeholder=" " id="date" />
-        <Input type="time" placeholder=" " id="time" />
+        <Input
+          type="date"
+          placeholder=" "
+          id="date"
+          value={userInputs.date}
+          onChange={e => handleChange("date", e.target.value)}
+        />
+        <Input
+          type="time"
+          placeholder=" "
+          id="time"
+          value={userInputs.time}
+          onChange={e => handleChange("time", e.target.value)}
+        />
         <div className="form_buttons">
           <Button type="submit" name="submit" />
           <Button type="reset" name="reset" />
